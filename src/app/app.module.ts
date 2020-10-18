3e24e04aaf43a6af5178bacf2de504380904eef0
dmithamo <dennisbmithamo@gmail.com>
2020-10-18 12:04:36 +0300

    feat(init): initialize an agular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CopyrightComponent } from './copyright/copyright.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CopyrightComponent
   ],
   imports: [
     BrowserModule,
