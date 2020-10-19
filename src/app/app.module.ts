dbd315b3b24e339c0035b79af63f43ec13389e2b
dmithamo <dennisbmithamo@gmail.com>
2020-10-19 09:35:54 +0300

    configure support for material ui framework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CopyrightComponent } from './copyright/copyright.component';
-import { AuthModule } from './auth/auth.module';
+import { CopyrightComponent } from './components/copyright/copyright.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { AuthModule } from './features/auth/auth.module';
+import { MaterialUIModule } from './material-ui/material-ui.module';
 
 @NgModule({
-  declarations: [AppComponent, CopyrightComponent],
-  imports: [BrowserModule, AppRoutingModule, AuthModule],
+  declarations: [AppComponent, CopyrightComponent, ToolbarComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule,
+    BrowserAnimationsModule,
+    MaterialUIModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
